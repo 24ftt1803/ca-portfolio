97ca264e6f417a4d22aab61ba76980cb3b3aa3d6
24ftt1803 <24ftt1803@student.pb.edu.bn>
2025-08-07 19:52:42 +0800

    Create DisplayPattern2.java

---
+++ p2/DisplayPattern2.java
@@ -0,0 +1,14 @@
+public class Display_Pattern2 {
+    public static void main(String[] args) {
+        System.out.println("   .=''''=.");
+        System.out.println("  /        \\");
+        System.out.println(" |  0    0  |");
+        System.out.println(" \\    /\\    /");
+        System.out.println(" /' _=\\/=_' \\");
+        System.out.println("/ /        \\ \\          CHIP CHIP!");
+        System.out.println("\\/\\        /\\/");
+        System.out.println("  ' .    . ' ");
+        System.out.println("   /|\\  /|\\");
+    
+}
+}
